719ddc9717680ba8bda57cd481fc6552bab03c70
amanzishan <amanzishan.az@gmail.com>
2019-12-20 11:30:30 +0530

    Updates

---
+++ precache-manifest.ee7f375cc4ac06eb2cb38fbb94c7ca2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e52734ea870c1840df50121577306da",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "383bfe19edaad4155fbc",
+    "url": "/portfolio/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "3880d908dbffc744a2af",
+    "url": "/portfolio/static/js/2.cc76ae63.chunk.js"
+  },
+  {
+    "revision": "34558ff2c0bdfe5372d1be52e7940c06",
+    "url": "/portfolio/static/js/2.cc76ae63.chunk.js.LICENSE"
+  },
+  {
+    "revision": "383bfe19edaad4155fbc",
+    "url": "/portfolio/static/js/main.583fc75e.chunk.js"
+  },
+  {
+    "revision": "4bcc3983bbdf3794ec1c",
+    "url": "/portfolio/static/js/runtime-main.6fcaddba.js"
+  }
+]);
